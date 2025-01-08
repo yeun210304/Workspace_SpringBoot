ec7c51b18747cf214c0436061e45ef1dd869e393
Yeun <80025908+yeun210304@users.noreply.github.com>
2025-01-08 16:02:11 +0900

    순수JDBC연결

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,34 @@
 package hello.hellospring;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+// import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    } 
+
     @Bean
     public MemberService memberService() {
-        return new MemberService(memMemberRepository());
+        return new MemberService(memberRepository());
     }
 
     @Bean
-    public MemoryMemberRepository memMemberRepository() {
-        return new MemoryMemberRepository();
+    public MemberRepository memberRepository() {
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
