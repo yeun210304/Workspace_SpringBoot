1d1f0767df39d2978ad0c05ae62e074cb75868f1
Yeun <80025908+yeun210304@users.noreply.github.com>
2025-01-10 16:13:36 +0900

    스프링 데이터 JPA. 설정부터 실행까지

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,34 +1,24 @@
 package hello.hellospring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-// import hello.hellospring.repository.JdbcMemberRepository;
-// import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
-import jakarta.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-        // return new MemoryMemberRepository();
-        // return new JdbcMemberRepository(dataSource);
-        // return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+        return new MemberService(memberRepository);
     }
 }
---
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,22 @@
+package hello.hellospring.repository;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import hello.hellospring.domain.Member  ;
+
+@SuppressWarnings("unchecked")
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    public Member save(Member member);
+
+    @Override
+    Optional<Member> findById(Long id);
+
+    @Override
+    Optional<Member> findByName(String name);
+
+    @Override
+    List<Member> findAll();
+}
