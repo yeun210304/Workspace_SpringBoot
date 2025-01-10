2d42bc99a559f5c2dfa20c14f2fe4faa3139e602
Yeun <80025908+yeun210304@users.noreply.github.com>
2025-01-10 14:00:28 +0900

    import삭제

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,6 +1,5 @@
 package hello.hellospring.domain;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
